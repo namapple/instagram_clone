20ceeaa83f8570090abeb8adb8802012bcf94a92
rs-anhnq2 <anhnq2@runsystem.net>
2021-12-31 21:50:09 +0700

    add todo

--- instagram_login_screen/lib/screens/login/login_screen.dart
+++ instagram_login_screen/lib/screens/login/login_screen.dart
@@ -12,6 +12,7 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
+    // TODO: (namdd) transfer declare material app to main.dart
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
