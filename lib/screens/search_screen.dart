5b19d54cc9168c6436ae6fff5f48a471e56d9e77
Nam Dao <bk.namapple@gmail.com>
2022-01-11 18:06:26 +0700

    add stories view & search screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
-
+import 'package:instagram_login_screen/models/search_model.dart';
+import 'package:instagram_login_screen/screens/search/categories.dart';
+import 'package:instagram_login_screen/screens/search/search_bar.dart';
 class Browse extends StatelessWidget {
-  const Browse({ Key? key }) : super(key: key);
+  const Browse({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Browse'),
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const BuildSearchBar(),
+              const SizedBox(height: 10),
+              const Catergories(),
+              const SizedBox(height: 10),
+              GridView.count(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                crossAxisCount: 3,
+                mainAxisSpacing: 2,
+                crossAxisSpacing: 2,
+                children: searchList
+                    .map((e) => Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: NetworkImage(e.image),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ))
+                    .toList(),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
