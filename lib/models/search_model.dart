651aead3d43727a5321a569c33bd10ff8be5f4ab
Nam Dao <bk.namapple@gmail.com>
2022-01-11 18:08:36 +0700

    add search screen

---
+++ lib/models/search_model.dart
@@ -0,0 +1,24 @@
+class Search {
+  Search({required this.image});
+
+  final String image;
+}
+
+
+List<Search> searchList = [
+  Search(image: 'https://picsum.photos/200'),
+  Search(image: 'https://picsum.photos/300'),
+  Search(image: 'https://picsum.photos/100'),
+  Search(image: 'https://picsum.photos/400'),
+  Search(image: 'https://picsum.photos/500'),
+  Search(image: 'https://picsum.photos/100'),
+  Search(image: 'https://picsum.photos/300'),
+  Search(image: 'https://picsum.photos/200'),
+  Search(image: 'https://picsum.photos/500'),
+  Search(image: 'https://picsum.photos/400'),
+  Search(image: 'https://picsum.photos/100'),
+  Search(image: 'https://picsum.photos/300'),
+  Search(image: 'https://picsum.photos/400'),
+  Search(image: 'https://picsum.photos/500'),
+  Search(image: 'https://picsum.photos/200'),
+];
